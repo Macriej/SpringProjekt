758f9805bb3e6cd8b19268c1440916da944fd381
maciej <macriej@gmail.com>
2018-05-31 22:48:52 +0200

    Third commit

--- src/main/java/babinski/maciej/spring/config/SecurityConfig.java
+++ src/main/java/babinski/maciej/spring/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/")
+                .antMatchers("/", "/webjars/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
